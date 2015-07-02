a934c09e59c889b9a7ae2e560425dc2f15bd83e1
Patrick Double <pat@patdouble.com>
2015-07-02 11:33:24 -0500

    Create TestPage1.java

---
+++ src/test/java/com/github/double16/TestPage1.java
@@ -0,0 +1,11 @@
+package com.github.double16;
+
+import org.openqa.selenium.WebDriver;
+
+import com.mutualofomaha.eps.test.functional.pages.AbstractPage;
+
+public class TestPage1 extends AbstractPage {
+    public TestPage1(WebDriver driver) {
+        super(driver);
+    }
+}
