94278b4657cae25ae1879789742858b1363163b2
Patrick Double <pat@patdouble.com>
2015-07-02 11:33:52 -0500

    Create TestPage2.java

---
+++ src/test/java/com/github/double16/TestPage2.java
@@ -0,0 +1,13 @@
+package com.github.double16;
+
+import org.openqa.selenium.WebDriver;
+
+import com.mutualofomaha.eps.test.functional.pages.AbstractPage;
+
+public class TestPage2 extends AbstractPage {
+    public static final String url = "page2";
+
+    public TestPage2(WebDriver driver) {
+        super(driver);
+    }
+}
